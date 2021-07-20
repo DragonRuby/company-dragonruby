75f6955d375201bbd78cb59a09f3e329fecc3d91
Amir Rajan <ar@amirrajan.net>
2021-07-20 08:45:54 -0500

    Update dragonruby.el

--- dragonruby.el
+++ dragonruby.el
@@ -1,6 +1,12 @@
 (require 'cl-lib)
 (require 'subr-x)
+(require 'browse-url)
+(require 'request)
 
+;; browse-url-url-encode-chars
+(setq company-backends (delete 'company-dragonruby company-backends))
+(setq company-backends (delete 'company-dragonruby company-backends))
+(setq company-backends (delete 'company-dragonruby company-backends))
 (setq company-backends (delete 'company-dragonruby company-backends))
 
 (defun company-dragonruby--buffer-text (buffer)
@@ -8,22 +14,22 @@
                        (save-restriction (widen)
                                          (buffer-substring-no-properties (point-min) (point-max)))))
 
-(defvar company-dragonruby--mailbox-code
+(defun company-dragonruby--mailbox-code-template ()
 "
-suggestions = $gtk.suggest_autocompletion index: :buffer-index, text: <<-S
+suggestions = $gtk.suggest_autocompletion index: :buffer-index, text: <<-COMPANY_DRAGONRUBY_BUFFERHEREDOC
 :buffer-text
-S
+COMPANY_DRAGONRUBY_BUFFERHEREDOC
 
 $gtk.write_file 'app/autocomplete.txt', suggestions.join(\"\\n\")
 "
 )
 
-(defun company-dragonruby--mailbox-code-for (index code)
+(defun company-dragonruby--mailbox-code (index code)
   (s-replace ":buffer-text"
              code
              (s-replace ":buffer-index"
                         (number-to-string index)
-                        company-dragonruby--mailbox-code)))
+                        (company-dragonruby--mailbox-code-template))))
 
 (defun company-dragonruby--read-autocomplete ()
   (if (file-exists-p "autocomplete.txt")
@@ -31,17 +37,53 @@ $gtk.write_file 'app/autocomplete.txt', suggestions.join(\"\\n\")
                         (split-string (buffer-string) "\n" t))
       '()))
 
+(defun amir-httpbin ()
+  (interactive)
+  (request "http://httpbin.org/put"
+  :type "PUT"
+  :data (json-encode '(("key" . "value") ("key2" . "value2")))
+  :headers '(("Content-Type" . "application/json"))
+  :parser 'json-read
+  :success (cl-function
+            (lambda (&key response &allow-other-keys)
+              (message "I sent: %S" (assoc-default 'json data))))) )
+
+(defun amir-try-request ()
+  (interactive)
+  (request "http://localhost:9001/dragon/autocomplete/"
+           :type "POST"
+           :data (json-encode '(("index" . "27") ("text" . "def tick args\n  args.state.\nend")))
+           :parser 'buffer-string
+           :error (cl-function
+                   (lambda (&rest args &key error-thrown &allow-other-keys)
+                     (message "Got error: %S" error-thrown)))
+           :sync t
+           :success
+           (cl-function (lambda (&key data &allow-other-keys)
+                          (when data
+                            (format "%s" data))))))
+
 (defun company-dragonruby--write-autocomplete ()
-  (let ((code (company-dragonruby--buffer-text (current-buffer)))
+  (interactive)
+  (let ((text (company-dragonruby--buffer-text (current-buffer)))
         (index (point)))
-    (let ((final-code (company-dragonruby--mailbox-code-for index code)))
-      (write-region final-code nil "./mailbox.txt")
-      (shell-command-to-string "ruby ./autocomplete-mri.rb"))))
+    (request "http://localhost:9001/dragon/autocomplete/"
+             :type "POST"
+             :data (json-encode `(("index" . ,index) ("text" . ,text)))
+             :parser 'buffer-string
+             :error (cl-function
+                     (lambda (&rest args &key error-thrown &allow-other-keys)
+                       (message "Got error: %S" error-thrown)))
+             :sync t
+             :success
+             (cl-function (lambda (&key data &allow-other-keys)
+                            (when data
+                              (setq company-dragonruby--autocomplete-result (format "%s" data))))))))
 
 (defun company-dragonruby--find-candidates (prefix)
-  (let (res)
+  (let ((res '()))
     (company-dragonruby--write-autocomplete)
-    (dolist (item (company-dragonruby--read-autocomplete))
+    (dolist (item (split-string company-dragonruby--autocomplete-result "\n" t))
       (when (string-prefix-p prefix item)
         (push (propertize item) res)))
     res))
