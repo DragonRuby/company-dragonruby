a8fe8e724b8c07b805b7657acf7f2105e1e93fb4
Amir Rajan <ar@amirrajan.net>
2024-07-11 12:28:24 -0500

    Update dragonruby.el

--- dragonruby.el
+++ dragonruby.el
@@ -14,29 +14,6 @@
                        (save-restriction (widen)
                                          (buffer-substring-no-properties (point-min) (point-max)))))
 
-(defun company-dragonruby--mailbox-code-template ()
-"
-suggestions = $gtk.suggest_autocompletion index: :buffer-index, text: <<-COMPANY_DRAGONRUBY_BUFFERHEREDOC
-:buffer-text
-COMPANY_DRAGONRUBY_BUFFERHEREDOC
-
-$gtk.write_file 'app/autocomplete.txt', suggestions.join(\"\\n\")
-"
-)
-
-(defun company-dragonruby--mailbox-code (index code)
-  (s-replace ":buffer-text"
-             code
-             (s-replace ":buffer-index"
-                        (number-to-string index)
-                        (company-dragonruby--mailbox-code-template))))
-
-(defun company-dragonruby--read-autocomplete ()
-  (if (file-exists-p "autocomplete.txt")
-      (with-temp-buffer (insert-file-contents "autocomplete.txt")
-                        (split-string (buffer-string) "\n" t))
-      '()))
-
 (defun amir-httpbin ()
   (interactive)
   (request "http://httpbin.org/put"
@@ -48,41 +25,28 @@ $gtk.write_file 'app/autocomplete.txt', suggestions.join(\"\\n\")
             (lambda (&key response &allow-other-keys)
               (message "I sent: %S" (assoc-default 'json data))))) )
 
-(defun amir-try-request ()
+(defun company-dragonruby--get-autocomplete ()
   (interactive)
-  (request "http://localhost:9001/dragon/autocomplete/"
-           :type "POST"
-           :data (json-encode '(("index" . "27") ("text" . "def tick args\n  args.state.\nend")))
-           :parser 'buffer-string
-           :error (cl-function
-                   (lambda (&rest args &key error-thrown &allow-other-keys)
-                     (message "Got error: %S" error-thrown)))
-           :sync t
-           :success
-           (cl-function (lambda (&key data &allow-other-keys)
-                          (when data
-                            (format "%s" data))))))
-
-(defun company-dragonruby--write-autocomplete ()
-  (interactive)
-  (let ((text (company-dragonruby--buffer-text (current-buffer)))
-        (index (point)))
-    (request "http://localhost:9001/dragon/autocomplete/"
-             :type "POST"
-             :data (json-encode `(("index" . ,index) ("text" . ,text)))
-             :parser 'buffer-string
-             :error (cl-function
-                     (lambda (&rest args &key error-thrown &allow-other-keys)
-                       (message "Got error: %S" error-thrown)))
-             :sync t
-             :success
-             (cl-function (lambda (&key data &allow-other-keys)
-                            (when data
-                              (setq company-dragonruby--autocomplete-result (format "%s" data))))))))
+  (let* ((text (company-dragonruby--buffer-text (current-buffer)))
+	 (index (point))
+	 (char-at-index-for-text (string (aref text (- index 2)))))
+    (if (string= char-at-index-for-text ".")
+	(request "http://localhost:9001/dragon/autocomplete/"
+	  :type "POST"
+	  :data (json-encode `(("index" . ,index) ("text" . ,text)))
+	  :parser 'buffer-string
+	  :error (cl-function (lambda (&rest args &key error-thrown &allow-other-keys)
+				(message "Got error: %S" error-thrown)))
+	  :sync t
+	  :success
+	  (cl-function (lambda (&key data &allow-other-keys)
+			 (when data
+			   (setq company-dragonruby--autocomplete-result (format "%s" data))))))
+      (setq company-dragonruby--autocomplete-result ""))))
 
 (defun company-dragonruby--find-candidates (prefix)
   (let ((res '()))
-    (company-dragonruby--write-autocomplete)
+    (company-dragonruby--get-autocomplete)
     (dolist (item (split-string company-dragonruby--autocomplete-result "\n" t))
       (when (string-prefix-p prefix item)
         (push (propertize item) res)))
